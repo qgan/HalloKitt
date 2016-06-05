1a5303f7275dd652799a0020fec73d53f84b36c4
Quan Gan <agandynamic@163.com>
2016-06-05 16:39:25 +0800

    try

---
+++ HalloKitt/HalloKitt/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HalloKitt
+//
+//  Created by QuanGan on 16/6/5.
+//  Copyright © 2016年 www.QuanGan.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
